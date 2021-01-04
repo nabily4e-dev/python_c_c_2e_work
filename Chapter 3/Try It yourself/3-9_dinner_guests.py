2fbdd8e00b08fa626589836d706e7a460b664da1
Nbl Dev <nbl.dev21@gmail.com>
2021-01-05 00:02:04 +0300

    Done with the  file 3-9_dinner_guests.py created file in try it your self exercise

---
+++ Chapter 3/Try It yourself/3-9_dinner_guests.py
@@ -0,0 +1,12 @@
+print("\n" + ">>>:"), print()#Space for convention only!
+############################################
+
+
+
+to_be_invited = ['Muhammad', 'Ahmad', 'Mansore']
+
+print(len(to_be_invited))
+
+
+############################################
+print('\n') #Space for convention only!
