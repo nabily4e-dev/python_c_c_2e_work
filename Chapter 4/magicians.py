045dfec98a2f6843f5a6ed9ab8af882da8813ac9
nabily4eDev <nbl.dev21@gmail.com>
2021-01-10 18:05:08 +0300

    Started working chapter 4 and created magicians.py file days ago ~_~

---
+++ Chapter 4/magicians.py
@@ -0,0 +1,77 @@
+print("\n" + ">>>:"), print()#Space for convention only!
+############################################
+
+
+
+message = ", that wat a great trick!"
+message_2 = "I can not wait to see the next trick"
+
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(f"{magician.capitalize()}{message}")
+    print(f"{message_2} {magician.capitalize()}.\n")
+
+
+print("Thank you, everyone. That was a great magic show!")
+
+
+# magicians = ['alice', 'david', 'carolina']
+# for magician in magicians:
+# print(magician)
+
+
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(f"{magician.title()}, that was a great trick!")
+print(f"I can't wait to see your next trick, {magician.title()}.\n")
+
+
+#    print()
+
+
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(f"{magician.title()}, that was a great trick!")
+    print(f"I can't wait to see your next trick, {magician.title()}.\n")
+
+    print("Thank you everyone, that was a great magic show!")
+
+
+# magicians = ['alice', 'david', 'carolina']
+# for magician in magicians
+#    print(magician)
+
+
+for value in range(1, 5):
+    print(value)
+
+for value in range(1, 6):
+    print(value)
+
+
+numbers = list(range(1, 6))
+print(numbers)
+
+
+even_numbers = list(range(2, 11, 2))
+print(even_numbers)
+
+
+squares = []
+for value in range(1, 11):
+    square = value ** 2
+    squares.append(square)
+
+print(squares)
+
+
+squares = []
+for value in range(1, 11):
+    squares.append(value ** 2)
+
+print(squares)
+
+
+
+############################################
+print('\n') #Space for convention only!
