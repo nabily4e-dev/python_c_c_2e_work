333766cbb1dd93cd2cf624da5b7047722050def7
nabily4e-dev <nbl.dev21@gmail.com>
2021-02-14 22:27:09 +0300

    Done woking!

---
+++ Chapter 4/Try It Yourself/4-2_animals.py
@@ -0,0 +1,23 @@
+print("\n" + ">>>:"), print()#Space for convention only!
+############################################
+
+
+animals = ['Lion', 'Tiger', 'Cheetah', 'Cat']
+r = 0
+
+for animal in animals[:3]:
+    r += 1
+    print(animal)
+    #print('A ' + animal[2] + 'would make a great pet')
+    
+    if r == 3:
+        print(f"A {animals[3]} would make a great pet")
+
+
+print('All animals are from cat type!')
+
+
+
+############################################
+print('\n') #Space for convention only!
+
