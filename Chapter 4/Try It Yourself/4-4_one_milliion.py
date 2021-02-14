74306fb5575297b63a9d4f754b02d1563cf9e37b
nabily4e-dev <nbl.dev21@gmail.com>
2021-02-15 02:25:26 +0300

    Creating and finishing work with 4-4_one_milliion.py file

---
+++ Chapter 4/Try It Yourself/4-4_one_milliion.py
@@ -0,0 +1,13 @@
+print("\n" + ">>>:"), print()#Space for convention only!
+############################################
+
+
+one_million = list(range(1, 1000001))
+
+for number in one_million:
+    print(one_million)
+
+
+
+############################################
+print('\n') #Space for convention only!
