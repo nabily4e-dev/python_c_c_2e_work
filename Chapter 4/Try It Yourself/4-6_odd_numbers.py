da5e364abc1a47be745bbb952e58e6ba77adc38d
nabily4e-dev <nbl.dev21@gmail.com>
2021-02-15 02:44:02 +0300

    Creating 4-6_odd_numbers.py file

---
+++ Chapter 4/Try It Yourself/4-6_odd_numbers.py
@@ -0,0 +1,10 @@
+print("\n" + ">>>:"), print()#Space for convention only!
+############################################
+
+
+odd_number
+
+
+
+############################################
+print('\n') #Space for convention only!
