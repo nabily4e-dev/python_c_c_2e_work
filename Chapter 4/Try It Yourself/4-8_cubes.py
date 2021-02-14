83032da39e386586535783de3739f7379e94518b
nabily4e-dev <nbl.dev21@gmail.com>
2021-02-15 02:59:14 +0300

    File 4-8_cubes.py Done!

---
+++ Chapter 4/Try It Yourself/4-8_cubes.py
@@ -0,0 +1,12 @@
+print("\n" + ">>>:"), print()#Space for convention only!
+############################################
+
+
+cubes = [value ** 3 for value in range(1, 11)]
+
+print(cubes)
+
+
+
+############################################
+print('\n') #Space for convention only!
