d91ce81fc2147e484d7881d23e7c7e216f7ec86b
nabily4e-dev <nbl.dev21@gmail.com>
2021-02-15 02:19:07 +0300

    Done with 4-3_counting_to_twenty.py file

---
+++ Chapter 4/Try It Yourself/4-3_counting_to_twenty.py
@@ -0,0 +1,10 @@
+print("\n" + ">>>:"), print()#Space for convention only!
+############################################
+
+
+for number in range(1, 21):
+    print(number)
+
+
+############################################
+print('\n') #Space for convention only!
