7b918aa8c53d33f8139a25ee0c594de8fc5e720e
nabily4e-dev <nbl.dev21@gmail.com>
2021-02-15 03:18:55 +0300

    players.py file finished working with

---
+++ Chapter 4/players.py
@@ -0,0 +1,27 @@
+print("\n" + ">>>:"), print()#Space for convention only!
+############################################
+
+
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+
+print(players[0:3])
+
+print(players[1:4])
+
+print(players[:4])
+
+print(players[2:])
+
+print(players[-3:])
+
+
+print("\n\nHere are the first three players on my team:")
+
+for player in players[:3]:
+    print(player.title())
+
+
+
+
+############################################
+print('\n') #Space for convention only!
