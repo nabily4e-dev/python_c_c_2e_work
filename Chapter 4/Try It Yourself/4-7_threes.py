8e08199ad4793cb2080cd8422dca57c6b67659d5
nabily4e-dev <nbl.dev21@gmail.com>
2021-02-15 03:08:04 +0300

    Fixed wrong expression!(a bug^^)

--- Chapter 4/Try It Yourself/4-7_threes.py
+++ Chapter 4/Try It Yourself/4-7_threes.py
@@ -2,7 +2,7 @@ print("\n" + ">>>:"), print()#Space for convention only!
 ############################################
 
 
-threes = [value ** 3 for value in range(1, 31)]
+threes = [value * 3 for value in range(3, 31)]
 
 print(threes)
 
