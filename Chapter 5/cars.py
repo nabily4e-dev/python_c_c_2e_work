ed29e7fa7b101cb5c6e855c323fe4ff11daff603
nabily4e-dev <nbl.dev21@gmail.com>
2021-02-15 13:29:13 +0300

    cars.py fil done

---
+++ Chapter 5/cars.py
@@ -0,0 +1,26 @@
+print("\n" + ">>>:"), print()#Space for convention only!
+############################################
+
+
+cars = ['audi', 'bmw', 'subaru', 'toyota']
+
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())
+    else:
+        print(car.title())
+
+
+car = 'bmw'
+print(car == 'bmw')
+
+car = 'Audi'
+print(car == 'audi')
+
+car = 'Audi'
+print(car.lower() == 'audi')
+
+
+
+############################################
+print('\n') #Space for convention only!
