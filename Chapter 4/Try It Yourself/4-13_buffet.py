77ef4887ad11b04992e591b5ab825cf08c1900da
nabily4e-dev <nbl.dev21@gmail.com>
2021-02-15 03:54:45 +0300

    File 4-13_buffet.py Done!

---
+++ Chapter 4/Try It Yourself/4-13_buffet.py
@@ -0,0 +1,22 @@
+print("\n" + ">>>:"), print()#Space for convention only!
+############################################
+
+
+#buffet = ['Kabsah', 'Mahshi', 'Zorbian', 'Mathgoot', 'Akdah']
+
+buffet_t = ('Kabsah', 'Mahshi', 'Zorbian', 'Mathgoot', 'Akdah')
+
+#buffet_t[2]= 2
+
+for meal in buffet_t:
+    print(meal)
+
+buffet_t = ('Kabsah', 'Mahshi', 'Zorbian')
+print('New menu:')
+
+for meal in buffet_t:
+    print(meal)
+
+
+############################################
+print('\n') #Space for convention only!
