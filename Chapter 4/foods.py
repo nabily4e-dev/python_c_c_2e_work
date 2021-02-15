4b8658ed2e0c34929384d39b1eb7583f95216a70
nabily4e-dev <nbl.dev21@gmail.com>
2021-02-15 03:26:25 +0300

    foods.py file created and done!

---
+++ Chapter 4/foods.py
@@ -0,0 +1,26 @@
+print("\n" + ">>>:"), print()#Space for convention only!
+############################################
+
+
+my_foods = ['pizza', 'falafel', 'carrot cake']
+
+friend_foods = my_foods[:]
+
+print("My favorite foods are:")
+print(my_foods)
+print("\nMy friend's favorite foods are:")
+print(friend_foods)
+
+my_foods.append('cannoli')
+friend_foods.append('ice cream')
+
+print("My favorite foods are:")
+print(my_foods)
+print("\nMy friend's favorite foods are:")
+print(friend_foods)
+
+
+
+
+############################################
+print('\n') #Space for convention only!
