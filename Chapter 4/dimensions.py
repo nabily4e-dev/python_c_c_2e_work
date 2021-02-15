22fa87001d0801118c78831331741ff0eea6a53d
nabily4e-dev <nbl.dev21@gmail.com>
2021-02-15 03:41:19 +0300

    dimensions.py file Done!

---
+++ Chapter 4/dimensions.py
@@ -0,0 +1,33 @@
+print("\n" + ">>>:"), print()#Space for convention only!
+############################################
+
+
+dimensions = (200, 50)
+
+print(dimensions[0])
+print(dimensions[1])
+
+# dimensions[0] = 250
+
+my_t = (3,)
+
+for dimension in dimensions:
+    print(dimension)
+
+
+dimensions = (200, 50).
+print("Original dimensions:")
+
+for dimension in dimensions:
+    print(dimension)
+
+dimensions = (400, 100)
+print("\nModified dimensions:")
+
+for dimension in dimensions:
+    print(dimension)
+
+
+
+############################################
+print('\n') #Space for convention only!
