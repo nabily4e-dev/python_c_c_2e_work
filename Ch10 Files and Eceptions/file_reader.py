b065707df7a89c8c75345158b17972c0778f459c
nabily4e <nbl.dev21@gmail.com>
2021-05-19 03:28:37 -0400

    Added .gitignore file and finished working with file_reader.py

--- Ch10 Files and Eceptions/file_reader.py
+++ Ch10 Files and Eceptions/file_reader.py
@@ -1,3 +1,30 @@
-with open('alice.txt') as file_object:
-    contents = file_object.read()
-print(contents)
+# with open('alice.txt') as fb:
+#     contents = fb.read()
+# print(contents)
+
+# print(), print()
+
+# with open('pi_digits.txt') as file_object:
+#     contents = file_object.read()
+# print(contents)
+
+# with open('/home/nabily4e/Desktop/Python crash course/python_c_c_2e_work/pi_digits.txt') as file_object:
+#     contents = file_object.read()
+# print(contents)
+
+# with open('./pi_digits.txt') as file_object:
+#     contents = file_object.read()
+# print(contents.rstrip())
+
+filename = 'pi_digits.txt'
+
+with open(filename)as file_object:
+    for line in file_object:
+        print(line.rstrip())
+
+filename = 'pi_digits.txt'
+with open(filename) as file_object:
+    lines = file_object.readlines()
+
+for line in lines:
+    print(line.rstrip())
